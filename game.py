0051b6d36a8e94da16ef5ceb9db10c10461107ed
BossUth <112574060+BossUth@users.noreply.github.com>
2023-01-26 12:27:43 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -43,7 +43,7 @@ class SNAKE:
 
 
             if index == 0:
-                screen.blit(self.head_up,block_rect)
+                screen.blit(self.head_right,block_rect)
             elif index == len(self.body) - 1:
                 screen.blit(self.tail,block_rect)
 
@@ -66,19 +66,19 @@ class SNAKE:
 
 
     def update_head_graphics(self):
-        head_relation = self.body[1] - self.body[0]
-        if head_relation == Vector2(1,0): self.head = self.head_left
-        elif head_relation == Vector2(-1,0): self.head = self.head_right
-        elif head_relation == Vector2(0,1): self.head = self.head_up
-        elif head_relation == Vector2(0,-1): self.head = self.head_down
+        head_position = self.body[1] - self.body[0]
+        if head_position == Vector2(1,0): self.head = self.head_left
+        elif head_position == Vector2(-1,0): self.head = self.head_right
+        elif head_position == Vector2(0,1): self.head = self.head_up
+        elif head_position == Vector2(0,-1): self.head = self.head_down
 
 
     def update_tail_graphics(self):
-        tail_relation = self.body[-2] - self.body[-1]
-        if tail_relation == Vector2(1,0): self.tail = self.tail_left
-        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
-        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
-        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
+        tail_position = self.body[-2] - self.body[-1]
+        if tail_position == Vector2(1,0): self.tail = self.tail_left
+        elif tail_position == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_position == Vector2(0,1): self.tail = self.tail_up
+        elif tail_position == Vector2(0,-1): self.tail = self.tail_down
             
 
     def move_snake(self):
@@ -110,7 +110,7 @@ class FRUIT:
     
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
-        screen.blit(block,fruit_rect)
+        screen.blit(apple,fruit_rect)
         pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
     def randomize(self):
@@ -130,9 +130,10 @@ class MAIN:
         self.check_fail()
 
     def draw_elements(self):
+        self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
-        self.draw_grass()
+        self.draw_score()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
@@ -173,10 +174,10 @@ class MAIN:
     
     def draw_score(self):
         score_text = str(len(self.snake.body) - 3)
-        score_surface = game_font.render(score_text,True,(56,74,12))
-        score_x = int(cell_size * cell_number -60)
-        score_y = int(cell_size * cell_number -40)
-        score_rect = score_surface.get_react(center =(score_x,score_y))
+        score_surface = game_font.render(score_text,False,(56,74,12))
+        score_x = int(cell_size * cell_number - 100)
+        score_y = int(cell_size * cell_number - 50)
+        score_rect = score_surface.get_rect(center = (score_x,score_y))
         apple_rect = apple.get_rect(midright = (score_rect.left,score_rect.centery))
 
 
@@ -188,15 +189,14 @@ class MAIN:
         pygame.draw.rect(screen,(56,74,12),bg_rect,2)
                  
     
-pygame.mixer.pre_init(44100,-16,2,512)
+pygame.mixer.pre_init(22050, -16, 2, 2048)
 pygame.init()
 cell_size = 30
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
-block = pygame.image.load('Graphics/apple.png').convert_alpha()
-game_font = pygame.font.Font(None,25)
-apple = 'apple'
+apple = pygame.image.load('Graphics/apple.jpg').convert_alpha()
+game_font = pygame.font.Font('Font/jap.ttf',25)
 
 
 
